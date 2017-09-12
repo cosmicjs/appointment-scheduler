6f3c0e94ea0ea1e3c33e6a019e29c7c494f17e04
mateocain <msc66@pitt.edu>
2017-09-11 23:44:55 -0400

    Fix keys

--- extensions/appointment-scheduler-extension/src/index.js
+++ extensions/appointment-scheduler-extension/src/index.js
@@ -9,10 +9,6 @@ window.React = React
 
 let url = QueryString.parse(location.search)
 
-url.bucket_slug = 'cosmicappointmentscheduler'
-url.write_key = 'zBSo9C1a0t7dvI9eeIjfYTAXVtxc4ikjrdzqIzhOhHOHIxKa8W'
-url.read_key = 'tVUQFCgRT9dxvxkz6y8MnWzuowspflXu52qv5CqqYHkZcqYEbt'
-
 const config = { bucket: {
     slug: url.bucket_slug,
     write_key: url.write_key,
